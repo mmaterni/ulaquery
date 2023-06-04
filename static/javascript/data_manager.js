4b601f1057117d672e8279b068f3baeda77de83b
giuseppe materni <gabrieletenebra@gmail.com>
2023-06-04 14:56:17 +0200

    04-06-23 (14:56)

--- static/javascript/data_manager.js
+++ static/javascript/data_manager.js
@@ -9,6 +9,7 @@ var dm_ = (function () {
   const DIR_ULA_DATA = `/ula_data`;
   const DIR_DATA_EXP = `${DIR_ULA_DATA}/data_export`;
   const DICT_FORM_PATH = `${DIR_DATA_EXP}/dictionary.ula.csv`;
+
   const DM = {
     dict_rows: [],
     load_dict: async function () {
@@ -32,5 +33,23 @@ var dm_ = (function () {
     },
   };
   return DM;
-  // })();
 }).call(this);
+  // })();
+
+/*
+my=(function(){
+ ...
+
+ return{
+  name1:nameInt1,
+  name2:nameInt2,
+  ...
+ }
+
+}).call(this);
+
+uso:
+my.name1(...);
+my.name2(,,)
+
+*/
