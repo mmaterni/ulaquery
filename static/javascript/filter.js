ea9f05663cb3c9ef2bc990fd242495461e8d2a46
marta materni <gabrieletenebra@gmail.com>
2023-06-18 00:16:10 +0200

    18-06-23 (00:16)

--- static/javascript/filter.js
+++ static/javascript/filter.js
@@ -102,6 +102,7 @@ var FLT = {
     this.select();
     const js = D_M.setQueryConditions();
     const rows = D_M.findRows(js);
+    // AAA ResultSet
     DictForm.build(rows);
     DictForm.show();
   }
@@ -163,7 +164,7 @@ var Where = {
 
     for (let k in VS.msd_attrs) {
       const row = VS.msd_attrs[k];
-      const pm=row[0];;
+      const pm = row[0];;
       const rs = row.slice(1);
       const h = rh(rs);
       jt.append(fh(pm, h));
@@ -787,5 +788,161 @@ var PosMsd = {
       a.classList.remove("select");
     }
     FLT.select();
+  }
+};
+
+var ResultSet = {
+  id: "resultsetid",
+  tr_selected: null,
+  wind: null,
+  rows: [],
+  open: function () {
+    this.build(D_M.dict_rows);
+    this.show();
+  },
+  build: async function (rows) {
+    const sp = "         ";
+    const menu = `
+<div class="menu_wnd" >
+<ul>
+<li>
+<a class="tipb" href="javascript:ResultSet.scroll_top()">Top
+<span class="tiptextb">Scroll Top</span>
+</a>
+</li>
+<li>
+<a class="tipb" href="javascript:ResultSet.scroll_bottom()">Bottom
+<span class="tiptextb">Scroll Bottom</span>
+</a>
+</li>
+<li>
+<a class="tipb" href="javascript:ResultSet.scroll_left()">Left
+<span class="tiptextb">Scroll Left</span>
+</a>
+</li>
+<li>
+<a class="tipb" href="javascript:ResultSet.scroll_right()">Right
+<span class="tiptextb">Scroll Right</span>
+</a>
+</li>
+
+<li>
+<a class="cmd" href="#">Help
+</a>
+</li>
+<li>${sp}</li>
+<li><a href="javascript:ResultSet.hide()">X</a></li>
+<li></li>
+</ul>
+</div>
+<div class="resultset">
+`;
+    let jt = UaJth();
+    jt.append(menu);
+
+    const heads = D_M.dict_heads;
+    const forms_len = 8;
+    const head_form = heads.slice(0, forms_len);
+    const heaD_Msd = heads.slice(forms_len);
+    // console.log(heads);
+    // console.log(head_form);
+    // console.log(heaD_Msd);
+    // elimina la prima riga
+    // this.rows.shift();
+
+    // head
+    jt.append(`<table><thead><tr>`);
+    let h = (d) => `<th><span>${d}</span></th>`;
+    jt.append(h, "N.");
+    for (const x of head_form) jt.append(h, x.toUpperCase());
+    for (const x of heaD_Msd) jt.append(h, x.toUpperCase())
+    jt.append(`</tr></thead><tbody>`);
+
+    // rowa
+    const lers = rows.length;
+    const r0 = (d) => `<td>${d}</td>`;
+    const r1 = (d) => `<td class="m">${d}</td>`;
+
+    const resulRow = (r) => {
+      const r0 = r.slice(0, 8);
+      let rst = Array.from(r0);
+      // attrs
+      let arr = r.slice(8, 26);
+      let s = arr.filter(x => x !== '').join(',');
+      rst.push(s);
+      // sigls
+      arr = r.slice(26, 30);
+      s = arr.filter(x => x !== '').join(',');
+      rst.push(s);
+      // locts
+      arr = r.slice(30, 35);
+      s = arr.filter(x => x !== '').join(',');
+      rst.push(s);
+      // datets
+      alls = r.slice(35);
+      s = arr.filter(x => x !== '').join(',');
+      rst.push(s);
+    }
+
+
+    const ler = rows.length > 0 ? rows[0].length : 0;
+
+    for (let i = 0; i < lers; i++) {
+      const row = rows[i];
+      jt.append("<tr>")
+      jt.append(r0, i);
+      for (let i = 0; i < forms_len; i++) jt.append(r0, row[i])
+      for (let i = forms_len; i < ler; i++) jt.append(r1, row[i])
+      jt.append("</tr>")
+    }
+
+    jt.append(`</tbody></table></div>`);
+
+    const html = jt.html();
+    if (!this.wind) {
+      this.wind = UaWindowAdm.create(this.id, "ulaquery_id");
+      this.setXY();
+      this.wind.drag();
+    }
+    this.wind.hide();
+    this.wind.setHtml(html);
+  },
+  show: async function (url) {
+    if (!this.wind) return;
+    wait_start();
+    await sleep(10);
+    this.wind.show();
+    wait_stop();
+  },
+  hide: function () {
+    if (!this.wind) return;
+    this.wind.hide(this.id);
+  },
+  setXY: function () {
+    const left = 5;
+    const top = 50;
+    this.wind.setXY(left, top, -1);
+  },
+  resetXY: function () {
+    this.wind.reset();
+    this.setXY();
+    this.show();
+  },
+  scroll_top: function () {
+    const x = this.wind.w;
+    const e = x.querySelector("div.resultset");
+    e.scrollTop = 0;
+  },
+  scroll_bottom: function () {
+    const e = this.wind.w.querySelector("div.resultset");
+    e.scrollTop = e.scrollHeight;
+  },
+  scroll_left: function () {
+    const e = this.wind.w.querySelector("div.resultset");
+    e.scrollLeft = 0;
+  },
+  scroll_right: function () {
+    const e = this.wind.w.querySelector("div.resultset");
+    e.scrollLeft = e.scrollWidth;
   }
 };
