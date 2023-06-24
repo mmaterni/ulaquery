2a6cbc0a36072005e6eff9fb0d3ef057704a7daf
marta materni <gabrieletenebra@gmail.com>
2023-06-24 12:15:42 +0200

    24-06-23 (12:15)

--- ulaserver.py
+++ ulaserver.py
@@ -1,32 +1,14 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-from pdb import set_trace
-# import pprint
 import os
 import argparse
-from ulalib.bottle import Bottle
-from ulalib.bottle import request, static_file
-# from ulalib.save_back import save_text_data_back
-# # from ulalib.w_data import *
-# from ulalib.save_back import save_corpus_data_back
-# from ulalib.update_data import UpdateData
-from ulalib.ulaquery_setting import *
+from bottle import Bottle
+from bottle import request, static_file
 
 app = Bottle()
 
 ROOT_PATH = '.'
-
-# def prn_path():
-#     print("")
-#     cwd = os.getcwd()
-#     abspath = os.path.abspath(__file__)
-#     dirname = os.path.dirname(abspath)
-#     basename = os.path.basename(abspath)
-#     print("cwd:%s" % cwd)
-#     print("abspath:%s" % abspath)
-#     print("dirname:%s" % dirname)
-#     print("basename:%s" % basename)
 
 
 def prn_params(rqs):
@@ -100,74 +82,19 @@ def server_static(filepath):
     return s
 
 
-# @app.route('/diff', method='GET')
-# def diff_text_corpus():
+# @app.route('/write/<filepath:path>', method='POST')
+# def write(filepath=""):
+#     data = request_data(request)
 #     try:
-#         # prn_params(request)
-#         pars = request_params(request)
-#         name = pars.get("name", None)
-#         if name is None:
-#             return ""
-#         text_name = os.path.basename(name)
-#         upd_data = UpdateData()
-#         upd_data.set_text_name(text_name)
-#         upd_data.read_corpus_form_csv()
-#         upd_data.read_text_form_csv()
-#         upd_data.set_text_flled_idx_lst()
-#         rows = upd_data.set_text_corpus_diff_lst()
-#         data = {"data": rows}
+#         # save_data(filepath,data)
+#         fpath = os.path.join(ROOT_PATH, filepath)
+#         fw = open(fpath, "wb")
+#         fw.write(data)
+#         fw.close()
+#         os.chmod(fpath, 0o777)
+#         # save_text_data_back(filepath)
 #     except IOError as e:
-#         msg = f"ERRORO diff_text_corpus()"
-#         raise Exception(f"{msg}\n{e}")
-#     return data
-
-
-@app.route('/write/<filepath:path>', method='POST')
-def write(filepath=""):
-    data = request_data(request)
-    try:
-        # save_data(filepath,data)
-        fpath = os.path.join(ROOT_PATH, filepath)
-        fw = open(fpath, "wb")
-        fw.write(data)
-        fw.close()
-        os.chmod(fpath, 0o777)
-        # save_text_data_back(filepath)
-    except IOError as e:
-        msg = f"ERRORO write()"
-        raise Exception(f"{msg}\n{e}")
-    return "1"
-
-
-# @app.route('/updatecorpus/<filepath:path>', method='POST')
-# def update_corpus(filepath=""):
-#     try:
-#         #aggiorna corpus.form.csv
-#         #         corpus.omogr.json
-#         save_corpus_data_back(CORPUS_NAME)
-#         text_name = os.path.basename(filepath)
-#         upd_data = UpdateData()
-#         upd_data.set_text_name(text_name)
-#         rows = upd_data.update_corpus_forms()
-#         # righe del corpus sovrascritte con valori diversi
-#         # form_text$form_corpus
-#         # row=form|formkey...$forma|formakey|..
-#         data = {"data": rows}
-#     except IOError as e:
-#         msg = f"ERRORO update_corpus()"
-#         raise Exception(f"{msg}\n{e}")
-#     return data
-
-
-# @app.route('/updatetext/<filepath:path>', method='POST')
-# def update_text(filepath=""):
-#     try:
-#         text_name = os.path.basename(filepath)
-#         upd_data = UpdateData()
-#         upd_data.set_text_name(text_name)
-#         upd_data.update_text_forms()
-#     except IOError as e:
-#         msg = f"ERRORO update_text()"
+#         msg = f"ERRORO write()"
 #         raise Exception(f"{msg}\n{e}")
 #     return "1"
 
